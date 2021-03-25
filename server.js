48e40387e8d3f55baaa391dc64be896873b934c3
Alan <alan.gio@gmail.com>
2021-03-24 22:17:17 -0300

    Update server add notes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // SERVER SIDE
 
 const httpServer = require("http").createServer();
+const notes = [];
+
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: '*',
@@ -8,15 +10,24 @@ const io = require("socket.io")(httpServer, {
   }
 });
 
-
 io.on('connection', (client) => {
+
+  io.sockets.emit('note', notes);
+
   client.on('commentToShow', (comment) => {
     console.log('comment receiving', comment);
     io.sockets.emit('comment', comment);
   });
 
+  client.on('noteToShow', (note) => {
+    console.log('note receiving', note);
+    notes.push(note);
+    console.log('notes sent', notes);
+    io.sockets.emit('note', notes);
+  });
+
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 io.listen(port);
 console.log('listening on port ', port);
